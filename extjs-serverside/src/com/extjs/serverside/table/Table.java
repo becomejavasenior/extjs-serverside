22c08d3cf9b8e4b62829481be3e9a9f0f4d94545
gzsombor <gzsombor@f4f44997-6738-0410-9776-ab45e426fe17>
2007-08-24 02:17:42 +0000

    refactor table to become RenderableContainer

--- extjs-serverside/src/com/extjs/serverside/table/Table.java
+++ extjs-serverside/src/com/extjs/serverside/table/Table.java
@@ -2,8 +2,20 @@ package com.extjs.serverside.table;
 
 import java.io.IOException;
 
-public abstract class Table {
+import com.extjs.serverside.form.RenderableContainer;
 
+public abstract class Table implements RenderableContainer{
+
+	private String fieldName;
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	
 	public abstract TableColumn[] getColumns();
 	
 	public abstract void renderData(TableDataRequest request) throws IOException;
